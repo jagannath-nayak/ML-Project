a76b405820a8e8c86091ede37f002c7e0d6c9097
Jagannath Nayak <jagannath08108nayak@gmail.com>
2025-01-17 20:05:26 +0530

    Deployment configuration

--- src/utils.py
+++ src/utils.py
@@ -1,62 +1,68 @@
 import os
 import sys
-
-import numpy as np 
 import pandas as pd
+import numpy as np
 import dill
-import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
-
 from src.exception import CustomException
+from xgboost import XGBRegressor
+from sklearn.base import BaseEstimator, RegressorMixin
+
+class SklearnCompatibleXGBRegressor(XGBRegressor, BaseEstimator, RegressorMixin):
+    @staticmethod
+    def __sklearn_tags__():
+        return {}
 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
-
-        with open(file_path, "wb") as file_obj:
-            pickle.dump(obj, file_obj)
-
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomException(e, sys)        
     
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
+        for model_name, model in models.items():
+            try:
+                print(f"Evaluating model: {model_name}")
+                para = param.get(model_name, {})
+                print(f"Parameters: {para}")
 
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
+                # Initialize GridSearchCV
+                gs = GridSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
 
-            model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
+                # Set the best parameters and retrain
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
 
-            #model.fit(X_train, y_train)  # Train model
+                # Predictions
+                y_train_pred = model.predict(X_train)
+                y_test_pred = model.predict(X_test)
 
-            y_train_pred = model.predict(X_train)
+                # Scoring
+                train_model_score = r2_score(y_train, y_train_pred)
+                test_model_score = r2_score(y_test, y_test_pred)
 
-            y_test_pred = model.predict(X_test)
+                report[model_name] = test_model_score
 
-            train_model_score = r2_score(y_train, y_train_pred)
-
-            test_model_score = r2_score(y_test, y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
+            except Exception as inner_e:
+                print(f"Error with model {model_name}: {inner_e}")
 
         return report
 
     except Exception as e:
         raise CustomException(e, sys)
-    
+
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
